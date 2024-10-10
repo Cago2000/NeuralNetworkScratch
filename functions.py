758d5e293cba6425c12e86ddf411e9d5506f7712
Cago2000 <147174299+Cago2000@users.noreply.github.com>
2024-10-10 23:23:56 +0200

    added backpropagation

--- functions.py
+++ functions.py
@@ -1,9 +1,20 @@
 from math import e
 
 
-def sigmoid(values):
-    result = list(map(lambda x: 1/(1+(e**-x)), values))
-    return result
+def sigmoid_list(values, bias):
+    return list(map(lambda x: sigmoid(x, bias), values))
+
+
+def sigmoid_derivative_list(values, bias):
+    return list(map(lambda x: sigmoid_derivative(x, bias), values))
+
+
+def sigmoid(x, bias):
+    return 1 / (1 + (e ** -(x - bias)))
+
+
+def sigmoid_derivative(x, bias):
+    return 1 - (1 / (1 + (e ** -(x - bias))))
 
 
 def relu():
@@ -13,3 +24,9 @@ def relu():
 def tanh():
     return 1
 
+
+def xor(a, b):
+    if a != b:
+        return 1
+    else:
+        return 0
--- main.py
+++ main.py
@@ -1,16 +1,22 @@
 import functions
 
-n1, n2 = 0.2, 0.4
-layer1 = [n1, n2]
+alpha = 0.05
+bias = 0.5
 
+x1, x2 = 1, 1
+layer1 = [x1, x2]  # 1x2
 w11, w12, w13, w21, w22, w23 = 0.6, 0.7, 0.5, 0.4, 0.9, 0.6
-weights1 = [[w11, w12, w13],
+weights1 = [[w11, w12, w13],  # 2x3
             [w21, w22, w23]]
+z1 = []
 
-w21, w22, w23, w31, w32, w33 = 0.7, 1.8, 0.9, 0.3, 0.3, 0.6
-weights2 = [[w21, w31],
-            [w22, w32],
-            [w23, w33]]
+layer2 = []
+
+w21, w22, w23 = 0.7, 1.8, 0.9
+weights2 = [[w21],
+            [w22],
+            [w23]]
+z2 = []
 
 
 def matrix_multiplication(layer, weights):
@@ -23,15 +29,60 @@ def matrix_multiplication(layer, weights):
 
 
 def forward_pass(layer, weights):
-    result = matrix_multiplication(layer, weights)
-    result = functions.sigmoid(result)
-    return result
+    z = matrix_multiplication(layer, weights)
+    result = list(map(lambda x: functions.sigmoid_derivative(x, bias), z))
+    return z, result
+
+
+def calculate_error(layer):
+    error = []
+    for layer_value in layer:
+        e = -(functions.sigmoid(layer_value, bias))
+        error.append(e)
+    return error
+
+
+def output_delta(z, layer):
+    delta = []
+    error = calculate_error(layer)
+    g = list(map(lambda x: functions.sigmoid_derivative(x, bias), z))
+    for g_val, error_val in zip(g, error):
+        delta.append(g_val * error_val)
+    return delta
+
+
+def backpropagation(z, weights, delta):
+    error = matrix_multiplication(delta, weights)
+    new_delta = []
+    g = list(map(lambda x: functions.sigmoid_derivative(x, bias), z))
+    for g_val, error_val in zip(g, error):
+        new_delta.append(g_val * error_val)
+    return new_delta
+    # backwards to neuron with derivative with input = e*w
+
+
+# TODO
+def adjust_weights(deltas, weights):
+    new_weights = []
+    for i in range(0, 2):
+        alpha_delta = []
+        for val in deltas[i]:
+            alpha_delta.append(-alpha * val)
+        weight_delta = matrix_multiplication(alpha_delta, weights[i - 1])
+        for j in enumerate(weights[i]):
+            weights[i][j] += weight_delta[j]
+        new_weights.append(weights[i])
+    return new_weights
 
 
 def main():
-    layer2 = forward_pass(layer1, weights1)
-    layer3 = forward_pass(layer2, weights2)
-    print(layer3)
+    z1, layer2 = forward_pass(layer1, weights1)
+    z2, layer3 = forward_pass(layer2, weights2)
+    delta3 = output_delta(z2, layer3)
+    delta2 = backpropagation(z1, weights2, delta3)
+    # ones since input does not have weights going into first neuron
+    delta1 = backpropagation([1, 1, 1], weights1, delta2)
+    print(delta1)
 
 
 if __name__ == "__main__":
