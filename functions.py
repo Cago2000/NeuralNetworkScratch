36d9db73083a7363edd3f1fb2840d89e79352661
Cago2000 <147174299+Cago2000@users.noreply.github.com>
2024-10-23 19:50:06 +0200

    overhauled pretty much everything, uses tanh as activation now, converges but to wrong values

--- functions.py
+++ functions.py
@@ -1,20 +1,20 @@
 from math import e
 
 
-def sigmoid_list(values: list, bias: float) -> list:
-    return list(map(lambda x: sigmoid(x, bias, 5), values))
+def sigmoid_list(values: list) -> list:
+    return list(map(lambda x: sigmoid(x), values))
 
 
-def sigmoid_derivative_list(values: list, bias: float) -> list:
-    return list(map(lambda x: sigmoid_derivative(x, bias, 5), values))
+def sigmoid_derivative_list(values: list) -> list:
+    return list(map(lambda x: sigmoid_derivative(x), values))
 
 
-def sigmoid(x: float, bias: float, stretch: float) -> float:
-    return 1 / (1 + (e ** -(stretch*(x - bias))))
+def sigmoid(x: float) -> float:
+    return 1 / (1 + (e ** -x))
 
 
-def sigmoid_derivative(x: float, bias: float, stretch: float) -> float:
-    return 1 - (1 / (1 + (e ** -(stretch*(x - bias)))))
+def sigmoid_derivative(x: float) -> float:
+    return sigmoid(x) * (1 - sigmoid(x))
 
 
 def relu(x: float) -> float:
@@ -22,7 +22,19 @@ def relu(x: float) -> float:
 
 
 def tanh(x: float) -> float:
-    return (e ** x - e ** -x) / (e ** x + e ** -x)
+    return (e**x - e**(-x)) / (e**x + e**(-x))
+
+
+def tanh_derivative(x: float) -> float:
+    return 1-(tanh(x)**2)
+
+
+def tanh_list(values: list) -> list:
+    return list(map(lambda x: tanh(x), values))
+
+
+def tanh_derivative_list(values: list) -> list:
+    return list(map(lambda x: tanh_derivative(x), values))
 
 
 def xor(a: int, b: int) -> int:
--- main.py
+++ main.py
@@ -1,33 +1,16 @@
 import random
-
 import functions
 
-alpha = 0.005
-input_size = 3
-hidden_size = 3
 
-
-def return_consistent_weights():
-    w1_11, w1_12, w1_13, w1_21, w1_22, w1_23, w1_31, w1_32, w1_33 = 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5
-    weights1 = [[w1_11, w1_12],
-                [w1_21, w1_22],
-                [w1_31, w1_32]]
-    w2_21, w2_22, w2_23 = 0.5, 0.5, 0.5
-    weights2 = [[w2_21],
-                [w2_22],
-                [w2_23]]
+def return_consistent_weights(input_size: int, hidden_size: int, value: float) -> tuple:
+    weights1 = [[value for _ in range(input_size)] for _ in range(hidden_size)]
+    weights2 = [value for _ in range(hidden_size)]
     return weights1, weights2
 
 
-def return_new_weights():
-    w1_11, w1_12, w1_13, w1_21, w1_22, w1_23, w1_31, w1_32, w1_33 = tuple(random.random() for _ in range(input_size * hidden_size))
-    weights1 = [[w1_11, w1_12, w1_13],
-                [w1_21, w1_22, w1_23],
-                [w1_31, w1_32, w1_33]]
-    w2_21, w2_22, w2_23 = tuple(random.random() for _ in range(hidden_size))
-    weights2 = [[w2_21],
-                [w2_22],
-                [w2_23]]
+def return_random_weights(input_size: int, hidden_size: int) -> tuple:
+    weights1 = [[random.random() for _ in range(input_size)] for _ in range(hidden_size)]
+    weights2 = [random.random() for _ in range(hidden_size)]
     return weights1, weights2
 
 
@@ -50,23 +33,23 @@ def matrix_multiplication(X: list, Y: list) -> list:
 
 def forward_pass(h: list, weights: list) -> tuple:
     z = matrix_multiplication(h, weights)
-    new_h = list(map(lambda x: functions.sigmoid(x, 0, 5), z[0]))
+    new_h = functions.tanh_list(z[0])
     return z, new_h
 
 
 def calculate_output_error(x1: int, x2: int, h: list) -> list:
     error = []
     for y_pred in h:
-        y_true = functions.sigmoid(functions.xor(x1, x2), 0, 5)
-        e = y_true - y_pred
+        y_true = functions.xor(x1, x2)
+        e = (y_true - y_pred)**2
         error.append(e)
+        #print(f'y_pred: {y_pred}, y_true: {y_true}, e: {e}')
     return error
 
 
-def output_delta(x1: int, x2: int, z: list, h: list) -> list:
+def output_delta(error: list, z: list) -> list:
     delta = []
-    error = calculate_output_error(x1, x2, [h])
-    derivative = list(map(lambda x: functions.sigmoid_derivative(x, 0, 5), z))
+    derivative = functions.tanh_derivative_list(z)
     for derivative_val, error_val in zip(derivative, error):
         delta.append(derivative_val * error_val)
     return delta
@@ -75,65 +58,80 @@ def output_delta(x1: int, x2: int, z: list, h: list) -> list:
 def backpropagation(z: list, weights: list, delta: list) -> list:
     error = matrix_multiplication([delta], weights)
     new_delta = []
-    derivative = list(map(lambda x: functions.sigmoid_derivative(x, 0, 5), z[0]))
+    derivative = functions.tanh_derivative_list(z[0])
     for derivative_val, error_val in zip(derivative, error[0]):
         new_delta.append(derivative_val * error_val)
     return new_delta
 
 
-def adjust_weights(weights: list, delta: list, layer: list) -> list:
-    weight_delta = tensor_product(delta, layer)
+def adjust_weights(weights: list, delta: list, h: list, alpha: float) -> list:
+    weight_delta = tensor_product(delta, h)
     adjusted_weights = weights.copy()
     index = 0
     for i, row in enumerate(weights):
-        for j, val in enumerate(row):
-            adjusted_weights[i][j] = weights[i][j] + (alpha * weight_delta[index])
+        for j, weight in enumerate(row):
+            adjusted_weights[i][j] += (alpha * weight_delta[index])
             index += 1
     return adjusted_weights
 
 
-def predict(input: list, weights1: list, weights2: list):
-    _, h2 = forward_pass(input, weights1)
-    z2, y = forward_pass([h2], weights2)
-    return y[0]
+def transpose_matrix(X: list):
+    result = [[0] * len(X)] * len(X[0])
+    for i, row in enumerate(X):
+        for j, val in enumerate(row):
+            result[j][i] = val
+    return result
 
 
-def fit(data: list, weights1: list, weights2: list):
-    weights1, weights2 = return_consistent_weights()  # return_new_weights()
-    for i in range(0, 10000):
+def fit(iterations: int, data: list, input_size: int, hidden_size: int, alpha: float) -> tuple:
+    weights1, weights2 = return_random_weights(input_size, hidden_size)
+    for i in range(0, iterations):
         for row in data:
             h1 = row
+            #print("h1:", h1)
             z1, h2 = forward_pass([h1], weights1)
-            z2, y = forward_pass([h2], weights2)
-            delta3 = output_delta(h1[0], h1[1], z2[0], y[0])
-            delta2 = backpropagation(z1, weights2, delta3)
-            delta1 = backpropagation([h1], weights1, delta2)
-            weights2 = adjust_weights(weights2, delta3, h2)
-            weights1 = adjust_weights(weights1, delta2, h1)
+            #print("z1:", z1)
+            #print("h2:", h2)
+            z2, y = forward_pass([h2], [weights2])
+            #print("z2:", z2)
+            #print("y:", y)
+            error = calculate_output_error(h1[0], h1[1], y)
+            delta3 = output_delta(error, z2[0])
+            #print("delta3:", delta3)
+            t_weights2 = transpose_matrix([weights2])
+            delta2 = backpropagation(z1, t_weights2, delta3)
+            #print("delta2:", delta2)
+            weights2 = adjust_weights([weights2], delta3, h2, alpha)
+            weights1 = adjust_weights(weights1, delta2, h1, alpha)
+            weights2 = weights2[0]
+
     return weights1, weights2
 
 
-print_stuff = True
+def predict(h1: list, weights1: list, weights2: list):
+    _, h2 = forward_pass(h1, weights1)
+    z2, y = forward_pass([h2], [weights2])
+    print(f'pred: {y[0]}, x1: {h1[0][0]}, x2: {h1[0][1]}, y: {functions.xor(h1[0][0], h1[0][1])}')
+
+
+def predict_all(samples: list, weights1: list, weights2: list):
+    print("--------------------Predict XOR--------------------")
+    for sample in samples:
+        predict([sample], weights1, weights2)
+    print("---------------------------------------------------\n")
+
 
 def main() -> None:
-    samples = [[-1, -1],
-               [ 1,  1],
-               [-1,  1],
-               [ 1, -1]]
+    samples = [[1, -1],
+               [-1, 1],
+               [1, 1],
+               [-1, -1]]
 
-    weights1, weights2 = return_consistent_weights()
-    if print_stuff:
-        print(predict([[-1, -1]], weights1, weights2), functions.xor(samples[0][0], samples[0][1]))
-        print(predict([[1, 1]], weights1, weights2), functions.xor(samples[1][0], samples[1][1]))
-        print(predict([[-1, 1]], weights1, weights2), functions.xor(samples[2][0], samples[2][1]))
-        print(predict([[1, -1]], weights1, weights2), functions.xor(samples[3][0], samples[3][1]))
-    weights1, weights2 = fit(samples, weights1, weights2)
-    if print_stuff:
-        print(predict([[-1, -1]], weights1, weights2), functions.xor(samples[0][0], samples[0][1]))
-        print(predict([[1, 1]], weights1, weights2), functions.xor(samples[1][0], samples[1][1]))
-        print(predict([[-1, 1]], weights1, weights2), functions.xor(samples[2][0], samples[2][1]))
-        print(predict([[1, -1]], weights1, weights2), functions.xor(samples[3][0], samples[3][1]))
+    samples2 = [[1, -1],
+                [-1, 1]]
 
+    weights1, weights2 = fit(iterations=10000, data=samples2, input_size=2, hidden_size=2, alpha=0.005)
+    predict_all(samples2, weights1, weights2)
 
 
 if __name__ == "__main__":
