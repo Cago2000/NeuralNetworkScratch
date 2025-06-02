04f9f615941f7d09d6ed01b7b272f42dd8268b0e
Cago2000 <147174299+Cago2000@users.noreply.github.com>
2025-06-03 01:31:27 +0200

    refactored forward pass, backpropagation and weight adjusting

--- neural_network.py
+++ neural_network.py
@@ -1,32 +1,41 @@
 import math
-from enums import Model, Act_Func
+from enums import Model
 import enums
 import functions
 from copy import deepcopy
 
 
-def forward_pass(h: list, weights: list, act_func: Act_Func) -> tuple:
-    z = functions.matrix_multiplication(h, weights)
-    new_h = act_func.get_function()(z[0])
-    return z, new_h
-
-def backpropagation(z: list, weights: list, delta: list, act_func: Act_Func) -> list:
-    new_delta = []
-    derivative = act_func.get_derivative_function()(z[0])
-    error = functions.matrix_multiplication(delta, weights)
-    for derivative_val, error_val in zip(derivative[0], error[0]):
-        new_delta.append(derivative_val * error_val)
-    return [new_delta]
+def forward_pass(h_list: list, weights: list, act_functions: list) -> tuple:
+    z_list = []
+    for w, act_func in zip(weights, act_functions):
+        z = functions.matrix_multiplication(h_list[-1], w)
+        h = act_func.get_function()(z[0])
+        h_list.append(h)
+        z_list.append(z)
+    return z_list, h_list
 
 
-def adjust_weights(weights: list, delta: list, h: list, alpha: float) -> list:
-    weight_delta = functions.tensor_product(delta[0], h)
-    adjusted_weights = deepcopy(weights)
-    index = 0
-    for i, row in enumerate(weights):
-        for j, _ in enumerate(row):
-            adjusted_weights[i][j] += (alpha * weight_delta[index])
-            index += 1
+def backpropagation(z_list: list, w_list: list, d_list: list, act_functions: list) -> list:
+    for z, w, d, act_func in zip(reversed(z_list), reversed(w_list), d_list, reversed(act_functions)):
+        derivative = act_func.get_derivative_function()(z[0])
+        error = functions.matrix_multiplication(d, functions.transpose_matrix(w))
+        new_delta = [d_val * e_val for d_val, e_val in zip(derivative[0], error[0])]
+        d_list.append([new_delta])
+    return d_list
+
+
+def adjust_weights(w_list: list, d_list: list, h_list: list, alpha: float) -> list:
+    adjusted_weights = deepcopy(w_list)
+    for k, (w, d, h) in enumerate(zip(reversed(w_list), d_list, reversed(h_list))):
+        if k == 0:
+            continue
+        idx = len(w_list) - 1 - k
+        weight_delta = functions.tensor_product(d[0], h)
+        index = 0
+        for i, row in enumerate(w):
+            for j, _ in enumerate(row):
+                adjusted_weights[idx][i][j] += alpha * weight_delta[index]
+                index += 1
     return adjusted_weights
 
 
@@ -43,23 +52,14 @@ def fit(iterations: int, iteration_update: int, data: list, layer_sizes: list,
         errors = []
         for j, row in enumerate(data):
             h_list = [[row]]
-            z_list = []
             d_list = []
-            for (k, h), w, act_func in zip(enumerate(h_list), w_list, act_functions):
-                if k < len(layer_sizes) - 1:
-                    z, h = forward_pass(h, w, act_func)
-                    h_list.append(h)
-                    z_list.append(z)
+            z_list, h_list = forward_pass(h_list, w_list, act_functions)
             error = model.get_error_function(h_list, y_train[j])
-            for err in error[0]:
-                errors.append(err ** 2)
+            errors.extend(err ** 2 for err in error[0])
             d_list.append(error)
-            for z, w, d, act_func in zip(reversed(z_list), reversed(w_list), d_list, reversed(act_functions)):
-                new_d = backpropagation(z, functions.transpose_matrix(w), d, act_func)
-                d_list.append(new_d)
-            for (k, w), d, h in zip(enumerate(reversed(w_list)), d_list, reversed(h_list)):
-                if k > 0:
-                    w_list[len(w_list) - 1 - k] = adjust_weights(w, d, h, alpha)
+            d_list = backpropagation(z_list, w_list, d_list, act_functions)
+            w_list = adjust_weights(w_list, d_list, h_list, alpha)
+
         met_threshold = True
         err_temp = 0.0
         for error in errors:
@@ -76,12 +76,8 @@ def fit(iterations: int, iteration_update: int, data: list, layer_sizes: list,
 
 
 def predict(h: list, w_list: list, model: Model, print_output: bool,
-            act_functions: list, layer_sizes: list, y_true: int):
-    h_list = [h]
-    for (k, h), w, act_func in zip(enumerate(h_list), w_list, act_functions):
-        if k < len(layer_sizes) - 1:
-            _, h = forward_pass(h, w, act_func)
-            h_list.append(h)
+            act_functions: list, y_true: int):
+    _, h_list = forward_pass([h], w_list, act_functions)
     y = h_list[-1]
     if print_output:
         match model:
@@ -106,10 +102,10 @@ def predict_all(samples: list, weights: list, model: Model, print_output: bool,
         match model:
             case Model.DIGIT:
                 predictions.append(predict([sample], weights, model, print_output,
-                                   act_functions, layer_sizes, y_true[i]))
+                                           act_functions, y_true[i]))
             case _:
                 predictions.append(predict([sample], weights, model, print_output,
-                                           act_functions, layer_sizes, 0))
+                                           act_functions, 0))
     if print_output:
         print("---------------------------------------------------\n")
     return predictions
