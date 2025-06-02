c7da0bc85564457db4d02b4d02711bbb8574475a
Cago2000 <147174299+Cago2000@users.noreply.github.com>
2025-06-03 01:13:49 +0200

    added cos functions, added error_function to models

--- enums.py
+++ enums.py
@@ -2,7 +2,6 @@ from enum import Enum
 import functions
 import math
 
-
 class Model(Enum):
     SIN = 0
     COS = 1
@@ -18,9 +17,16 @@ class Model(Enum):
             case self.XOR:
                 return xor
             case self.DIGIT:
-                return functions.get_digit_error
+                return get_digit_error
             case _:
                 return None
+
+    def get_error_function(self, h_list, y_labels):
+        match self:
+            case Model.DIGIT:
+                return get_digit_error(h_list[-1], y_labels)
+            case _:
+                return calculate_output_error(h_list[0], h_list[-1], self)
 
 
 class Act_Func(Enum):
@@ -63,6 +69,9 @@ class Act_Func(Enum):
 
 def xor(a: int, b: int) -> int:
     return 1 if a != b else -1
+
+def xor_list(values: list) -> list:
+    return [(lambda row: xor(row[0], row[1]))(row) for row in values]
 
 def sigmoid_list(values: list) -> list:
     return [list(map(lambda x: sigmoid(x), values))]
@@ -123,4 +132,36 @@ def sin_list(values: list) -> list:
 
 def sin_derivative_list(values: list) -> list:
     return [list(map(lambda x: sin_derivative(x), values))]
+
+def cos(x: float) -> float:
+    return math.cos(x)
+
+def cos_derivative(x: float) -> float:
+    return -math.sin(x)
+
+def cos_list(values: list) -> list:
+    return [list(map(lambda x: cos(x), values))]
+
+def cos_derivative_list(values: list) -> list:
+    return [list(map(lambda x: cos_derivative(x), values))]
+
+
+def calculate_output_error(x: list, h: list, model: Model) -> list:
+    x = x[0][:-1] if len(x[0]) > 1 else x[0]
+    error = []
+    for y_pred in h[0]:
+        y_true = model.get_function()(*x)
+        e = y_true - y_pred
+        error.append(e)
+    return [error]
+
+
+def get_digit_error(y: list, y_true: int) -> list:
+    error = []
+    digit = [0.0]*10
+    digit[y_true] = 1
+    for i, y_pred in enumerate(y[0]):
+        err = digit[i] - y_pred
+        error.append(err)
+    return [error]
 
