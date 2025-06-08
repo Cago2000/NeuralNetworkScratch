1d86140bd17ea2936f8908b264d8e3c4e3c73dc8
Cago2000 <147174299+Cago2000@users.noreply.github.com>
2025-06-08 14:38:03 +0200

    changed run_model fieldnames, removing bias term for plotting

--- main.py
+++ main.py
@@ -9,8 +9,8 @@ from matplotlib import pyplot as plt
 def run_model(name: str,
               model: Model,
               sample_data: list,
-              x_values: list,
-              y_values: list,
+              x_train: list,
+              y_train: list,
               layer_sizes: list,
               act_functions: list,
               iterations: int,
@@ -19,7 +19,7 @@ def run_model(name: str,
               error_threshold: float,
               seed: int,
               plot_prediction: bool,
-              test_data: list):
+              x_test: list):
     weights, errors = nn.fit(
         iterations=iterations,
         iteration_update=iteration_update,
@@ -29,12 +29,12 @@ def run_model(name: str,
         error_threshold=error_threshold,
         model=model,
         act_functions=act_functions,
-        y_train=y_values,
+        y_train=y_train,
         seed=seed
     )
 
     predictions = nn.predict_all(
-        test_data, weights, model, True, act_functions, y_values
+        x_test, weights, model, True, act_functions, y_train
     )
 
     plt.plot(errors)
@@ -43,15 +43,15 @@ def run_model(name: str,
     plt.xlabel('Iteration')
     plt.show()
 
-    if plot_prediction and x_values is not None:
-        plt.plot(x_values, predictions)
+    if plot_prediction and x_test is not None:
+        x_test = [row[:-1] for row in x_test]  # remove bias term for plotting
+        plt.plot(x_test, predictions)
         plt.title(f'Model: {name} Prediction')
         plt.ylabel('Y')
         plt.xlabel('X')
         plt.show()
 
     return predictions, errors
-
 
 
 def main() -> None:
@@ -68,8 +68,8 @@ def main() -> None:
     run_model(name="XOR",
               model=Model.XOR,
               sample_data=xor_sample,
-              x_values=xor_x,
-              y_values=xor_y,
+              x_train=xor_x,
+              y_train=xor_y,
               layer_sizes=[3, 3, 1],
               act_functions=[Act_Func.TANH, Act_Func.IDENTITY],
               iterations=10000,
@@ -78,7 +78,7 @@ def main() -> None:
               error_threshold=1e-8,
               seed=42,
               plot_prediction=False,
-              test_data=xor_sample)
+              x_test=xor_sample)
 
     sin_bias = 1.0
     sin_x = list(numpy.linspace(0, 7, 300))
@@ -88,8 +88,8 @@ def main() -> None:
     run_model(name="SIN",
               model=Model.SIN,
               sample_data=sin_sample,
-              x_values=sin_x,
-              y_values=sin_y,
+              x_train=sin_x,
+              y_train=sin_y,
               layer_sizes=[2, 3, 1],
               act_functions=[Act_Func.SIN, Act_Func.IDENTITY],
               iterations=2000,
@@ -98,7 +98,7 @@ def main() -> None:
               error_threshold=1e-5,
               seed=42,
               plot_prediction=True,
-              test_data=sin_test)
+              x_test=sin_test)
 
     cos_bias = math.pi / 2
     cos_x = list(numpy.linspace(0, 7, 300))
@@ -108,8 +108,8 @@ def main() -> None:
     run_model(name="COS",
               model=Model.COS,
               sample_data=cos_sample,
-              x_values=cos_x,
-              y_values=cos_y,
+              x_train=cos_x,
+              y_train=cos_y,
               layer_sizes=[2, 3, 1],
               act_functions=[Act_Func.SIN, Act_Func.IDENTITY],
               iterations=5000,
@@ -118,7 +118,7 @@ def main() -> None:
               error_threshold=0.01,
               seed=42,
               plot_prediction=True,
-              test_data=cos_test)
+              x_test=cos_test)
 
 
 if __name__ == "__main__":
